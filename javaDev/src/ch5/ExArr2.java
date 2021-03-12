5460fc1fcdc09864b45e34c177578cf84237dd4d
hoyouni <sgm00006@gmail.com>
2021-03-12 11:58:29 +0900

    chap04_Array_ex2

--- javaDev/src/ch5/ExArr2.java
+++ javaDev/src/ch5/ExArr2.java
@@ -7,15 +7,17 @@ public class ExArr2 {
 		int[] answer = {1, 4, 4, 3, 1, 4, 4, 2, 1, 3, 2};
 		int[] counter = new int[4];
 		
+		System.out.println(Arrays.toString(counter));
+		
 		for(int i =0; i < answer.length; i++) {
 			counter[answer[i] - 1]++;
+			//System.out.println("counter : " + Arrays.toString(counter));
 		}
-		System.out.println("counter : " + Arrays.toString(counter) + "\n");
+		//System.out.println("counter : " + Arrays.toString(counter) + "\n");
 		
 		for(int i = 0; i < counter.length; i++) {
-			int cnt = counter[i];
-			System.out.print(cnt);
-			for(int j = 0; j < cnt; j++) {
+			System.out.print(counter[i]);
+			for(int j = 0; j < counter[i]; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
